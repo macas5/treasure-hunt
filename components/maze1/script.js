24b0c98b7329b963b9c5470ffcdc80e1ee8ae053
macas5 <mkalinauskas1993@gmail.com>
2022-09-01 17:24:02 +0300

    change movement length

--- components/maze1/script.js
+++ components/maze1/script.js
@@ -9,14 +9,14 @@ const eligibleMove = () => {
     if (position[1] <= 125) eligibleMoves.splice(indexUp, 1);
 
     const indexRight = eligibleMoves.indexOf('right')
-    if (position[0] >= 435) eligibleMoves.splice(indexRight, 1);
+    if (position[0] >= 425) eligibleMoves.splice(indexRight, 1);
 
     const indexDown = eligibleMoves.indexOf('down')
     if (position[1] >= 425) eligibleMoves.splice(indexDown, 1);
 
     const indexLeft = eligibleMoves.indexOf('left')
     if (position[0] <= 125) eligibleMoves.splice(indexLeft, 1);
-    
+
     return eligibleMoves;
 };
 
@@ -27,20 +27,20 @@ const move = (key) => {
     console.log(eligibleMove());
     switch (key) {
         case 'ArrowUp':
-            if (eligibleMove().includes('up')) pirate.style.top = (top - 5) + 'px';
+            if (eligibleMove().includes('up')) pirate.style.top = (top - 28) + 'px';
             else pirate.style.top = top +'px';
             break;
 
         case 'ArrowRight':
-            if (eligibleMove().includes('right')) pirate.style.left = (left + 5) + 'px';
+            if (eligibleMove().includes('right')) pirate.style.left = (left + 28) + 'px';
             break;
 
         case 'ArrowDown':
-            if (eligibleMove().includes('down')) pirate.style.top = (top + 5) + 'px';
+            if (eligibleMove().includes('down')) pirate.style.top = (top + 28) + 'px';
             break;  
 
         case 'ArrowLeft':
-            if (eligibleMove().includes('left')) pirate.style.left = (left - 5) + 'px';
+            if (eligibleMove().includes('left')) pirate.style.left = (left - 28) + 'px';
             break;
    
         default:
