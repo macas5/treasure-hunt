4aee2323667786229220f637706561c2060c5447
macas5 <mkalinauskas1993@gmail.com>
2022-09-01 17:05:50 +0300

    add outer move limit

--- components/maze1/script.js
+++ components/maze1/script.js
@@ -1,25 +1,47 @@
 const maze = document.querySelector("#maze-img");
 const pirate = document.querySelector('#pirate-img');
 
+const eligibleMove = () => {
+    const position = [pirate.offsetLeft, pirate.offsetTop]
+    const eligibleMoves = ['up', 'right', 'down', 'left'];
+
+    const indexUp = eligibleMoves.indexOf('up')
+    if (position[1] <= 125) eligibleMoves.splice(indexUp, 1);
+
+    const indexRight = eligibleMoves.indexOf('right')
+    if (position[0] >= 435) eligibleMoves.splice(indexRight, 1);
+
+    const indexDown = eligibleMoves.indexOf('down')
+    if (position[1] >= 425) eligibleMoves.splice(indexDown, 1);
+
+    const indexLeft = eligibleMoves.indexOf('left')
+    if (position[0] <= 125) eligibleMoves.splice(indexLeft, 1);
+    
+    return eligibleMoves;
+};
+
+
 const move = (key) => {
     const top = pirate.offsetTop - 8;
     const left = pirate.offsetLeft - 8;
+    console.log(eligibleMove());
     switch (key) {
-        case 'ArrowRight':
-            pirate.style.left = (left + 5) + 'px';
-            break;
-
-        case 'ArrowLeft':
-            pirate.style.left = (left - 5) + 'px';
-            break;
-
         case 'ArrowUp':
-            pirate.style.top = (top - 5) + 'px';
+            if (eligibleMove().includes('up')) pirate.style.top = (top - 5) + 'px';
+            else pirate.style.top = top +'px';
+            break;
+
+        case 'ArrowRight':
+            if (eligibleMove().includes('right')) pirate.style.left = (left + 5) + 'px';
             break;
 
         case 'ArrowDown':
-            pirate.style.top = (top + 5) + 'px';
+            if (eligibleMove().includes('down')) pirate.style.top = (top + 5) + 'px';
             break;  
+
+        case 'ArrowLeft':
+            if (eligibleMove().includes('left')) pirate.style.left = (left - 5) + 'px';
+            break;
    
         default:
             break;
