f8a0532ec0207bc1029a1f2765766cc5931e2ba2
Niseme <nijolesemele@gmail.com>
2022-09-01 18:36:42 +0300

    grid with for loop

---
+++ index.js
@@ -0,0 +1,9 @@
+import { maps } from './mapPicturesData.js';
+import { testing } from "./mapPictures.js";
+import { gridContainer } from "./gridContainer.js";
+import {headerElement} from "./header.js"
+
+headerElement();
+gridContainer();
+testing(maps);
+
---
+++ mapPicturesData.js
@@ -0,0 +1,7 @@
+export const maps = [
+    'https://picsum.photos/200/200',
+    'https://picsum.photos/200/200',
+    'https://picsum.photos/200/200',
+    'https://picsum.photos/200/200',
+]
+
