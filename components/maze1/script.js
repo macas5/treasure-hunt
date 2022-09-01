0f15b68788b6c5f58314db064bcd1cfc51ee8f6a
macas5 <mkalinauskas1993@gmail.com>
2022-09-01 16:00:47 +0300

    remove unnecessary console.log

--- components/maze1/script.js
+++ components/maze1/script.js
@@ -1,7 +1,5 @@
 const maze = document.querySelector("#maze-img");
 const pirate = document.querySelector('#pirate-img');
-const activeKeys = [];
-let isRunning = true;
 
 const move = (key) => {
     const top = pirate.offsetTop - 8;
@@ -9,27 +7,21 @@ const move = (key) => {
     switch (key) {
         case 'ArrowRight':
             pirate.style.left = (left + 5) + 'px';
-            console.log('move right');
             break;
 
         case 'ArrowLeft':
             pirate.style.left = (left - 5) + 'px';
-            console.log('move left');
             break;
 
         case 'ArrowUp':
             pirate.style.top = (top - 5) + 'px';
-            console.log('move up');
             break;
 
         case 'ArrowDown':
             pirate.style.top = (top + 5) + 'px';
-            console.log('move down');
             break;  
    
         default:
-            console.log('invalid move');
-            
             break;
     }
 }
