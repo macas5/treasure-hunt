c9a8009b9c9007a352095c9216a93e53022f8c09
Niseme <nijolesemele@gmail.com>
2022-09-05 14:49:25 +0300

    map pictures

--- components/main-page/main-page-js/mapPicturesData.js
+++ components/main-page/main-page-js/mapPicturesData.js
@@ -1,24 +1,20 @@
 export const maps = [
     {
-        pic: 'https://cdn2.vectorstock.com/i/1000x1000/22/81/halloween-backgrounds-kids-playing-in-scary-vector-27132281.jpg',
+        pic: 'https://media.istockphoto.com/vectors/pirate-treasure-map-on-ruined-old-parchment-vector-id1237808740?k=20&m=1237808740&s=612x612&w=0&h=Gy7tz1eck4oGkks-lGTWApTrmSknDa5ezMD0pp42y6s=',
         link: './components/map1/map1.html',
-        filter: 'spooky'
     },
     {
         pic: 'https://t3.ftcdn.net/jpg/03/20/52/22/240_F_320522234_wIfcZ5ony9FNL123YgVnGDFe7xhnzZ4m.jpg',
-        link: 'https://www.example.com',
-        filter: 'pirates'
+        link: './components/maze/maze.html',
     },
     {
-        pic: 'https://img.freepik.com/premium-vector/wild-african-animals-jungle-picture-kids_145790-1414.jpg?w=2000',
-        link: 'https://www.delfi.lt',
-        filter: 'monkey'
+        pic: 'https://images5.alphacoders.com/842/842738.png',
+        link: './components/map2/map2.html',
 
     },
     {
-        pic: 'https://happylearning.tv/wp-content/uploads/2016/10/the-bear.jpg',
-        link: 'https://www.15min.lt',
-        filter: 'bear'
+        pic: 'https://s3.amazonaws.com/kzs3files/kidzsearchgl-fish-makeover-ks120414-159657-img.png',
+        link: './components/fishes/fishes.html',
     }
     
 ]
