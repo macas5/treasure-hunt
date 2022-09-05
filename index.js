c6433ae13b6e555a39617cc035891e79a839fa9a
Niseme <nijolesemele@gmail.com>
2022-09-05 14:17:01 +0300

    test commit

--- index.js
+++ index.js
@@ -4,10 +4,13 @@ import { mapPictures } from "./mapPictures.js";
 import { gridContainer } from "./gridContainer.js";
 import { headerElement } from "./header.js";
 import { btnElement } from "./btn.js";
+import { filter } from "./filter.js";
 
 
 headerElement();
+filter(maps)
 gridContainer();
 mapPictures(maps);
 btnElement(maps);
+
 
--- mapPicturesData.js
+++ mapPicturesData.js
@@ -1,20 +1,24 @@
 export const maps = [
     {
         pic: 'https://cdn2.vectorstock.com/i/1000x1000/22/81/halloween-backgrounds-kids-playing-in-scary-vector-27132281.jpg',
-        link: 'https://www.google.com',
+        link: './components/map1/map1.html',
+        filter: 'spooky'
     },
     {
         pic: 'https://t3.ftcdn.net/jpg/03/20/52/22/240_F_320522234_wIfcZ5ony9FNL123YgVnGDFe7xhnzZ4m.jpg',
         link: 'https://www.example.com',
+        filter: 'pirates'
     },
     {
         pic: 'https://img.freepik.com/premium-vector/wild-african-animals-jungle-picture-kids_145790-1414.jpg?w=2000',
         link: 'https://www.delfi.lt',
+        filter: 'monkey'
 
     },
     {
         pic: 'https://happylearning.tv/wp-content/uploads/2016/10/the-bear.jpg',
         link: 'https://www.15min.lt',
+        filter: 'bear'
     }
     
 ]
